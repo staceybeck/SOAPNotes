02eb40808d3bf8657a241e45b9fdeddeace6fce3
yecatstevir <staceyrivetbeck@gmail.com>
2023-02-24 12:28:09 -0800

    change logo size

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="tester" style={{textAlign:"center"}}>
-        <img src={logo} className="App-logo" alt="logo" width="400"
-     height="200" />
+        <img src={logo} className="App-logo" alt="logo" width="200"
+     height="100" />
         <Speaky />
       </header>
     </div>
