e4a7138ec8a72e8162a8123be025a7278a422dab
yecatstevir <staceyrivetbeck@gmail.com>
2023-02-24 21:30:48 -0800

    adding loading icon

--- src/speaky.js
+++ src/speaky.js
@@ -5,6 +5,7 @@ import SpeechRecognition, {
 import "./App.css";
 import ky from "ky";
 import microPhoneIcon from "./microphone.png";
+import { Oval } from 'react-loading-icons';
 
 const Speaky = () => {
   const { transcript, resetTranscript } = useSpeechRecognition();
@@ -12,6 +13,7 @@ const Speaky = () => {
   const [textEnabled, setTextEnabled] = useState(false);
   const [isListening, setIsListening] = useState(false);
   const microphoneRef = useRef(null);
+  const [loading, setLoading] = useState(false);
   if (!SpeechRecognition.browserSupportsSpeechRecognition) {
     return (
       <div className="mircophone-container">
@@ -42,6 +44,7 @@ const Speaky = () => {
   };
   const handleOutput = async () => {
     setNote(null);
+    setLoading(true);
     //json output
     const data = await ky
       .post("https://soapnotes-development-service.onrender.com/analyze", {
@@ -51,6 +54,7 @@ const Speaky = () => {
       })
       .json();
     setNote(data.text);
+    setLoading(false);
     setTextEnabled(true);
   };
   const handleText = async () => {
@@ -89,16 +93,16 @@ const Speaky = () => {
           </button>
         }
         {
-          <button className="generative-outputs btn" onClick={handleOutput}>
-            Generate Note
+          <button className="generative-outputs btn"
+                  onClick={handleOutput}>
+            {loading ? <Oval stroke="#fff"
+                             strokeWidth="10"
+                             height="20px"
+                             width="20px"/> :"Generate Note"}
           </button>
         }
         {
-          <button
-            className="output-text btn"
-            onClick={handleText}
-            disabled={!textEnabled}
-          >
+          <button className="output-text btn" onClick={handleText} disabled={!textEnabled}>
             Text Note
           </button>
         }
